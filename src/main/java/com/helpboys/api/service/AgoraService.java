7e650aa3145b2a5d03f78b2eafe3eee5f8fd1c6e
youngilkim0818 <youngil4449@gmail.com>
2026-03-28 23:21:41 +0900

    [fix] Agora 공식 AccessToken2 소스 적용 (커스텀 구현 제거)

--- src/main/java/com/helpboys/api/service/AgoraService.java
+++ src/main/java/com/helpboys/api/service/AgoraService.java
@@ -1,7 +1,7 @@
 package com.helpboys.api.service;
 
 import com.helpboys.api.dto.AgoraTokenResponse;
-import com.helpboys.api.util.AgoraTokenUtil;
+import io.agora.media.AccessToken2;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -19,10 +19,18 @@ public class AgoraService {
     private static final int TOKEN_EXPIRE_SECONDS = 3600; // 1시간
 
     public AgoraTokenResponse generateToken(String channelName, Long userId) {
-        int uid = 0; // uid=0이면 어떤 UID로든 채널 입장 가능
         try {
-            String token = AgoraTokenUtil.buildRtcToken(appId, appCertificate, channelName, uid, TOKEN_EXPIRE_SECONDS);
-            return new AgoraTokenResponse(token, channelName, uid);
+            AccessToken2 accessToken = new AccessToken2(appId, appCertificate, TOKEN_EXPIRE_SECONDS);
+            AccessToken2.ServiceRtc serviceRtc = new AccessToken2.ServiceRtc(channelName, "");
+
+            serviceRtc.addPrivilegeRtc(AccessToken2.PrivilegeRtc.PRIVILEGE_JOIN_CHANNEL, TOKEN_EXPIRE_SECONDS);
+            serviceRtc.addPrivilegeRtc(AccessToken2.PrivilegeRtc.PRIVILEGE_PUBLISH_AUDIO_STREAM, TOKEN_EXPIRE_SECONDS);
+            serviceRtc.addPrivilegeRtc(AccessToken2.PrivilegeRtc.PRIVILEGE_PUBLISH_VIDEO_STREAM, TOKEN_EXPIRE_SECONDS);
+            serviceRtc.addPrivilegeRtc(AccessToken2.PrivilegeRtc.PRIVILEGE_PUBLISH_DATA_STREAM, TOKEN_EXPIRE_SECONDS);
+            accessToken.addService(serviceRtc);
+
+            String token = accessToken.build();
+            return new AgoraTokenResponse(token, channelName, 0);
         } catch (Exception e) {
             throw new RuntimeException("Agora 토큰 생성 실패: " + e.getMessage());
         }
